4734a23ab2d547b61c527c038aa73e739daa8fbd
amgarton47 <amgb2018@mymail.pomona.edu>
2021-01-11 18:16:32 -0800

    changed express port to heroku environment

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const app = express();
-const PORT = 4747;
+const PORT = process.env.PORT || 4747;
 
 // static middleware
 app.use(express.static(path.join(__dirname, "../public")));
